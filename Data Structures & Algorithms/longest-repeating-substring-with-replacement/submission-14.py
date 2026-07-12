f4f4236682ec10d5e91fc95547b688b9e83a97c3
WoothAmwar <46137788+WoothAmwar@users.noreply.github.com>
2026-07-12 10:57:49 -0500

    Add: longest-repeating-substring-with-replacement - submission-14

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-14.py
@@ -0,0 +1,35 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l, r = 0, 0
+
+        characters = set(list(s))
+
+        max_size = 0
+
+        for ch in characters:
+            l, r = -1, -1  # l is exclusive, r are inclusize
+            # l is the first character which isn't included
+            #. r is the last character which is included
+            left = k
+            last_ch = -1
+
+            for idx, st in enumerate(list(s)):
+                if st == ch:
+                    r += 1
+                else:
+                    left -= 1
+                    if left >= 0:
+                        r += 1
+                    else:
+                        l += 1
+                        while list(s)[l] == ch:
+                            l += 1
+                        left = 0
+                        r += 1
+                
+                r = max(r, l)
+                print(ch, st, l, r, left)
+
+                max_size = max(r-l, max_size)
+        print(l, r)
+        return max_size
