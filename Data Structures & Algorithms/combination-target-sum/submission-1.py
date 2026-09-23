68d41efa994969da364274f039409dd9e8d22f71
WoothAmwar <46137788+WoothAmwar@users.noreply.github.com>
2026-09-23 16:32:43 -0500

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        sols = [[]]
+        true_sols = []
+
+        for n in nums:
+            for subset in sols:
+                if sum(subset+[n]) > target:
+                    continue
+                elif sum(subset+[n]) == target:
+                    true_sols.append(subset+[n])
+                # if n == nums[-1]:
+                #     if sum(subset+[n]) == target:
+                #         true_sols.append(subset+[n])
+                sols += [subset + [n]]
+        # for s in sols:
+        #     if sum(s) == target:
+        #         true_sols.append(s)
+        return true_sols
